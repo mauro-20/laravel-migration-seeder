27f4391789670a99d201e05e726b95f67428f1e7
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-11-09 21:14:52 +0100

    first commit

---
+++ database/migrations/2021_11_09_194619_create_packages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('destination', 50);
+            $table->unsignedTinyInteger('nights_number');
+            $table->date('departure');
+            $table->string('accomodation', 100);
+            $table->boolean('flight');
+            $table->unsignedSmallInteger('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
